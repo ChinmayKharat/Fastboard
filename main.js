5f2971e7ccfd4c24a8dbdcd224d5693776e09706
Chinmay_Kharat <chinmaykharat85@gmai.com>
2023-08-17 14:57:52 +0530

    Fastboard

---
+++ main.js
@@ -0,0 +1,29 @@
+import { createFastboard, mount } from "@netless/fastboard";
+
+let app;
+async function mountFastboard(div) {
+    
+    app = await createFastboard({
+        sdkConfig: {
+            appIdentifier: "y69tMDqPEe6qg3_eoBUomQ/AHdW4z1QWXS0Xg",
+            region: "us-sv",
+        },
+        joinRoom: {
+            uid: "User ID",
+            uuid: "029e6cf03a9111ee9e6241209ace6cf9",
+            roomToken: "NETLESSROOM_YWs9ZE5XVTFueGlHeGc4ZjRyRiZub25jZT0xNjkyMjU2NzEzNTM3MDAmcm9sZT0wJnNpZz00MWMxYTk0MGE3ZTM3MGViODYwNjI1YWYwZTBhZDBjOGFhYzc3ZWM1ZTBiYmJjNWE4ODc4NzEyOTc3ZDc4NTRkJnV1aWQ9MDI5ZTZjZjAzYTkxMTFlZTllNjI0MTIwOWFjZTZjZjk",
+        },
+        managerConfig: {
+            cursor: true,
+            
+        },
+    });
+    window.app = app;
+    return mount(app, div);
+}
+
+mountFastboard(document.getElementById("app"));
+await app.insertImage(
+    "https://web-cdn.agora.io/website-files/images/interactive-whiteboard-text-tab-1.png"
+);
+
